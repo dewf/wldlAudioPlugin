a5d97670997347c4d4ce421e7a21ac6ad59ea1e2
Daniel G <daniel@Daniels-Mac-10.9.local>
2019-04-16 01:59:21 -0600

    resizing + painting!!

--- cocoa_au2/AudioUnitEffectExample/Source/CocoaUI/AppleDemoFilter_OpenWLDL.mm
+++ cocoa_au2/AudioUnitEffectExample/Source/CocoaUI/AppleDemoFilter_OpenWLDL.mm
@@ -6,6 +6,8 @@
 #import <AudioUnit/AudioUnit.h>
 #import <AudioToolbox/AudioToolbox.h>
 
+#include <stdio.h>
+
 int eventCallback(wlWindow window, struct WLEvent *event, void *userData);
 
 static bool initFrameworks() {
@@ -13,34 +15,51 @@ static bool initFrameworks() {
     wlOpts.pluginSlaveMode = true; // essential! otherwise it will collide with the host program and everything breaks
     wlInit(eventCallback, &wlOpts);
     
-//    DLPlatformOptions dlOpts;
-//    dlOpts.reserved = 0;
-//    dl_Init(&dlOpts);
+    DLPlatformOptions dlOpts = {};
+    dl_Init(&dlOpts);
     
     return true;
 }
 
 class DemoFilterView
 {
+    int width;
+    int height;
 public:
-    DemoFilterView();
+    DemoFilterView(int width, int height);
     ~DemoFilterView();
+    inline int getHeight() { return height; }
     
-    void onEvent(void *inObject, const AudioUnitEvent *inEvent, Float32 inValue);
+    void onAUEvent(void *inObject, const AudioUnitEvent *inEvent, Float32 inValue);
+    void repaint(dl_CGContextRef context);
+    void resize(int newWidth, int newHeight);
 };
 
 void AUEventDispatcher(void *inRefCon, void *inObject, const AudioUnitEvent *inEvent, UInt64 inHostTime, Float32 inValue)
 {
-    auto dfvObject = (DemoFilterView *)inRefCon;
-    dfvObject->onEvent(inObject, inEvent, inValue);
+    auto filterView = (DemoFilterView *)inRefCon;
+    filterView->onAUEvent(inObject, inEvent, inValue);
 }
 
 int eventCallback(wlWindow window, struct WLEvent *event, void *userData)
 {
-    auto dfvObject = (DemoFilterView *)userData;
+    auto filterView = (DemoFilterView *)userData;
     
     event->handled = true;
     switch (event->eventType) {
+        case WLEventType_WindowResized:
+            filterView->resize(event->resizeEvent.newWidth, event->resizeEvent.newHeight);
+            break;
+            
+        case WLEventType_WindowRepaint: {
+            auto platformContext = (CGContextRef)event->repaintEvent.platformContext;
+            auto context = dl_CGContextCreateQuartz(platformContext, filterView->getHeight());
+            
+            filterView->repaint(context);
+            
+            dl_CGContextRelease(context);
+            break;
+        }
         default:
             event->handled = false;
     }
@@ -56,37 +75,59 @@ NSView *AppleDemoFilter_CreateWLViewFor(AudioUnit inAU, NSSize size)
     }
     
     if (inAU) {
-        auto dfvObject = new DemoFilterView;
+        auto intWidth = 900; //(int)size.width;
+        auto intHeight = 300; //(int)size.height;
+        printf("suggested size: %d,%d\n", (int)size.width, (int)size.height);
+        auto filterView = new DemoFilterView(intWidth, intHeight);
         
         wlWindow dummyWindow;
-        auto view = wlWindowCreateNSViewOnly((int)size.width, (int)size.height, dfvObject, &dummyWindow);
-        if (view) {
-            auto mask = NSViewWidthSizable | NSViewHeightSizable | NSViewMinXMargin |
-                            NSViewMaxXMargin | NSViewMinYMargin | NSViewMaxYMargin;
-            [view setAutoresizingMask:mask];
+        auto ns_view = wlWindowCreateNSViewOnly(intWidth, intHeight, filterView, &dummyWindow);
+        if (ns_view) {
             
             // attach various callbacks and such
             AUEventListenerRef auEventListener;
-            AUEventListenerCreate(AUEventDispatcher, dfvObject,
+            AUEventListenerCreate(AUEventDispatcher, filterView,
                                   CFRunLoopGetCurrent(), kCFRunLoopDefaultMode, 0.05, 0.05,
                                   &auEventListener);
             
-            return view;
+            return ns_view;
         }
     }
     return NULL;
 }
-    
-DemoFilterView::DemoFilterView()
-{
-    // nothing yet
-}
-DemoFilterView::~DemoFilterView()
-{
-    // nothing yet
-}
-void DemoFilterView::onEvent(void *inObject, const AudioUnitEvent *inEvent, Float32 inValue)
+
+DemoFilterView::DemoFilterView(int width, int height)
+    :width(width), height(height)
 {
     // nothing yet
 }
 
+DemoFilterView::~DemoFilterView()
+{
+    // nothing yet
+}
+
+void DemoFilterView::repaint(dl_CGContextRef context)
+{
+    dl_CGContextSaveGState(context);
+    
+    dl_CGContextSetRGBFillColor(context, 0, 0, 0.3, 1);
+    dl_CGContextFillRect(context, dl_CGRectMake(0, 0, width, height));
+    
+    dl_CGContextSetRGBStrokeColor(context, 1, 1, 0, 1);
+    dl_CGContextSetLineWidth(context, 2);
+    dl_CGContextStrokeRect(context, dl_CGRectMake(10, 10, width - 20, height - 20));
+    
+    dl_CGContextRestoreGState(context);
+}
+
+void DemoFilterView::resize(int newWidth, int newHeight)
+{
+    width = newWidth;
+    height = newHeight;
+}
+
+void DemoFilterView::onAUEvent(void *inObject, const AudioUnitEvent *inEvent, Float32 inValue)
+{
+    // nothing yet
+}
